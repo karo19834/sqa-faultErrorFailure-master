3d51076fc40734b1ea305b9a79ba1cc32586026b
karo19834 <karolina1983_4@hotmail.com>
2023-03-09 14:36:08 +0100

    rr

--- sqa-faultErrorFailure-master/src/at/campus02/example/TestFaultOne.java
+++ sqa-faultErrorFailure-master/src/at/campus02/example/TestFaultOne.java
@@ -16,4 +16,30 @@ public class TestFaultOne {
     }
 
 
+    @Test
+    public void zeroAtEnd(){
+        //Fault (Bug) vorhanden
+        //Error vorhanden: i startet bei 1, nicht bei 0
+        //Failure: nein (weil Resultat troztdem,stimmt)
+        int res = FaultOne.numZero(new int []{1,2,3,0});
+        Assert.assertEquals(1,res);
+    }
+
+    @Test
+    public void zeroAtStart(){
+        //Fault (Bug) vorhanden
+        //Error vorhanden: i startet bei 1, nicht bei 0
+        //Failure: nein (weil Resultat troztdem, stimmt)
+        int res = FaultOne.numZero(new int []{0,1,2,3});
+        Assert.assertEquals(1,res);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void expectException(){
+        //Fault (Bug) vorhanden
+        //Error nicht vorhanden: Exception wird vorher ausgelöst(mit if abfrage)
+        //Failure: nein
+        int res = FaultOne.numZero(null);
+        //we never reach this line
+    }
 }
